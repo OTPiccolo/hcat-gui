e94b318750b8ee689391c36f4c0ef2017e0a82cb
OT Piccolo <OT_Piccolo@web.de>
2019-09-01 11:09:47 +0200

    Creating a working RCP.

--- net.emb.hcat.gui.product/src/net/emb/hcat/gui/product/Activator.java
+++ net.emb.hcat.gui.product/src/net/emb/hcat/gui/product/Activator.java
@@ -1,44 +1,29 @@
 package net.emb.hcat.gui.product;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * Activator
+ *
+ * @author Heiko Mattes
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "net.emb.hcat.gui.product"; //$NON-NLS-1$
+	private static BundleContext context;
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
+	static BundleContext getContext() {
+		return context;
 	}
 
 	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
+	public void start(final BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
 	}
 
 	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
+	public void stop(final BundleContext bundleContext) throws Exception {
+		Activator.context = null;
 	}
 
 }
